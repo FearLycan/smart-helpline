a45b0cc1d5cfcf650ba83e79e9af8d95203c7995
DamianBronczyk <damian.bronczyk@gmail.com>
2017-06-25 14:08:21 +0200

    first commit

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=smart-helpline',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+];
